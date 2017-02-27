56bd6d7d7cdbd2d2a7b7dcaa4fb01b0bbe872b1e
tianxing <1035273486@qq.com>
2017-02-27 15:56:42 +0800

    添加测试代码

---
+++ src/test/java/com/tianxing/gradle/LockTest.java
@@ -0,0 +1,61 @@
+package com.tianxing.gradle;
+
+import org.junit.Test;
+
+import java.util.concurrent.*;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * Created by tianxing on 17/2/27.
+ *
+ */
+public class LockTest {
+
+    Object lock = new Object();
+    String string = "aa";
+    ReentrantLock reentrantLock = new ReentrantLock();
+
+    Executor executor = Executors.newScheduledThreadPool(5);
+    ScheduledExecutorService scheduledExecutorService = Executors.newScheduledThreadPool(5);
+
+
+
+    @Test
+    public void test(){
+        reentrantLock.lock();
+        reentrantLock.unlock();
+        synchronized (string){
+
+        }
+    }
+
+
+
+    private class Result implements Future<String>{
+
+        @Override
+        public boolean cancel(boolean mayInterruptIfRunning) {
+            return false;
+        }
+
+        @Override
+        public boolean isCancelled() {
+            return false;
+        }
+
+        @Override
+        public boolean isDone() {
+            return false;
+        }
+
+        @Override
+        public String get() throws InterruptedException, ExecutionException {
+            return null;
+        }
+
+        @Override
+        public String get(long timeout, TimeUnit unit) throws InterruptedException, ExecutionException, TimeoutException {
+            return null;
+        }
+    }
+}
